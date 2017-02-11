a7795b2140c5b30e7944663ed9bab2cd5f6fec01
Warner <rampy@vt.edu>
2017-02-11 16:40:47 -0500

    (3) ready for model submission 1: OLR interest_level~Price

--- (1) Ingestion, basic feature engineering.R
+++ (1) Ingestion, basic feature engineering.R
@@ -5,17 +5,23 @@ g <- glimpse
 
 fileLocation <- "C://Users/Warner/Desktop/Projects/Kaggle - Rental Listing Inquiries/train.json/train.json"
 
-
 # proper way to load data in from kaggle post: 
 # -- https://www.kaggle.com/danjordan/two-sigma-connect-rental-listing-inquiries/how-to-correctly-load-data-into-r/discussion
+danjRead <- function(directory){
+  packages <- c("jsonlite","dplyr","purrr")
+  purrr::walk(packages, library, character.only = TRUE, warn.conflicts = FALSE)
+  
+  data <- fromJSON(directory)
+  
+  # unlist every variable except `photos` and `features` and convert to tibble
+  vars <- setdiff(names(data), c("photos", "features"))
+  data <- map_at(data, vars, unlist) %>% tibble::as_tibble(.)
+}
 
-purrr::walk(packages, library, character.only = TRUE, warn.conflicts = FALSE)
+full <- danjRead(fileLocation)
 
-full <- fromJSON(fileLocation)
-
-# unlist every variable except `photos` and `features` and convert to tibble
-vars <- setdiff(names(full), c("photos", "features"))
-full <- map_at(full, vars, unlist) %>% tibble::as_tibble(.)
+# code target variable appriopriately as ordinal factor
+full$interest_level <- factor(full$interest_level, c("low","medium","high"))
 
 # create a few basic columns
 full$numPhotos <- sapply(full$photos, length)
@@ -30,3 +36,4 @@ full$created <- as.POSIXct(full$created, "%Y-%m-%d %H:%M:%S")
 full$LowTrafficDay <- weekdays(as.Date(full$created))
 full$LowTrafficDay <- ifelse(full$LowTrafficDay %in% c("Sunday","Monday"),
                              "Low", "Regular")
+
--- (3) Fit models.R
+++ (3) Fit models.R
@@ -1,14 +1,32 @@
-install.packages("rms", dependencies = TRUE) 
-
 
 # ! ------------------------------------------------------------
 # subsequent scripts from (1) rely on (1)'s final "full" table
 # ! ------------------------------------------------------------
 
-require("MASS")
+
 require("caret")
 require("rms") # installing required dependences = T
 
+danjRead <- function(directory){
+  packages <- c("jsonlite","dplyr","purrr")
+  purrr::walk(packages, library, character.only = TRUE, warn.conflicts = FALSE)
+  
+  data <- fromJSON(directory)
+  
+  # unlist every variable except `photos` and `features` and convert to tibble
+  vars <- setdiff(names(data), c("photos", "features"))
+  data <- map_at(data, vars, unlist) %>% tibble::as_tibble(.)
+  data
+}
+
+# read in test data 
+testDataLocation <- "C:/Users/Warner/Desktop/Projects/Kaggle - Rental Listing Inquiries/test.json/test.json"
+TEST <- danjRead(testDataLocation)
+
+# -----------------------------------------------------------------
+# fit basic ordinal logistic regression model to part of test data
+# ----------------------------------------------------------------
+# --
 # split data into train and test partitions using caret
 tr <- 0.7
 
@@ -16,16 +34,45 @@ tr <- 0.7
 trainIndex <- createDataPartition(full$interest_level,
                                   p = tr, list = FALSE, times = 1)
 
+full$interest_level <- as.factor(full$interest_level)
+
+
 # subset original data to get train and test sets
 train <- full[trainIndex,]
 test <- full[-trainIndex,]
 
 # target variable has to be coded as a factor for OLR
-train$interest_level <- as.factor(train$interest_level)
 
 # basic ordinal logistic regression with "Price" as predictor
-olr1 <- rms::lrm(interest_level ~ price, 
-                 data = train)
+olr1 <- lrm(interest_level ~ price, data = train)
 
-  
-  
+olr1 # model info
+
+# generate predictions 
+predictions <- predict(olr1, test, type="fitted")
+predictions <- data.frame(predictions)
+names(predictions) <- c("medium","high")
+
+# calculate low column
+predictions$low <- 1 - (predictions$medium + predictions$high)
+
+# -------------------------------------------------------------
+# fit basic ordinal logistic regression model for ALL test data
+# -------------------------------------------------------------
+
+# basic ordinal logistic regression with "price" as predictor
+OLR1 <- lrm(interest_level ~ price, data = full)
+
+PRDS <- predict(OLR1, TEST, type = "fitted")
+
+PRDS <- data.frame(PRDS)
+names(PRDS) <- c("medium","high")
+
+# calculate low column
+PRDS$low <- 1 - (PRDS$medium + PRDS$high)
+
+PRDS$listing_id <- TEST$listing_id
+
+PRDS <- PRDS[c("listing_id","high","medium","low")]
+
+write.csv(PRDS, "submission1.csv", row.names = FALSE)
